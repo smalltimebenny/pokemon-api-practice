e3ea4324cdd27f8dabab4222a5577b0212ecab18
smalltimebenny <ben.lindquist@gmail.com>
2022-11-17 19:10:32 -0500

    add component folder

---
+++ src/components/PokeNames.jsx
@@ -0,0 +1,22 @@
+import React, {useEffect, useState} from 'react';
+
+const PokeNames = () => {
+    const [moniker, setMoniker] = useState("")
+
+    useEffect (() => {
+        console.log("Names componenent loaded.")
+        fetch ("https://pokeapi.co/api/v2/pokemon/")
+            .then(response => {return response.json()})
+            .then(response => {setMoniker({name: response.results})})
+    }, []);
+
+    return (
+        <>
+            {moniker.name ? moniker.name.map((item, index) =>{
+                return(<div key={index}>{item.name}</div>)
+            }):null}
+        </>
+    )
+}
+
+export default PokeNames;
